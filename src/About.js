f37d7af92ffae5a893191fe2e0cc6ede5578392d
avr87 <annavanruiten@gmail.com>
2023-05-05 18:21:06 +0100

    added the contact component

--- src/About.js
+++ src/About.js
@@ -10,11 +10,20 @@ export default function About(){
             Hi, I'm junior front-end developer based in Londen with a career in
             the Musicals. I have performed in several musicals on tour as well
             as on the West-End. Currently I have found an interest in coding and
-            decided to give it a try. Following SheCodes's Max course I have
-            been introduced and practiced in HTML, CSS, Javascript, Bootstrap,
-            GitHub, React, Hosting, API, VSCode. I have thoroughly enoyed the
-            course and am hoping to find a job implementing these new learned
-            skills.
+            decided to give it a try. Following{" "}
+            <a
+              href="https://www.shecodes.io/"
+              target="_blank"
+              rel="noreferrer"
+              className="link"
+            >
+              {" "}
+              SheCodes
+            </a>
+            's Max course I have been introduced and practiced in HTML, CSS,
+            Javascript, Bootstrap, GitHub, React, Hosting, API, VSCode. I have
+            thoroughly enoyed the course and am hoping to find a job
+            implementing these new learned skills.
           </p>
         </div>
       </div>
--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,44 @@
 import React from "react";
+import "./Contact.css"
+import EmailLogo from "./emailLogo.png";
+import GithubLogo from "./githubLogo.png";
 
 export default function Contact(){
-    return(
-        <div id="contact">
+    return (
+      <div className="container">
+        <div className="contact">
+          <div id="contact">
+            <h2>I'd love to hear from you!</h2>
+                <h4>Let's get to know eachother</h4>
 
+            <div className="d-flex justify-content-evenly">
+              <button
+                type="button"
+                className="btn btn-light button contactButton"
+              >
+                <a
+                  href="mailto:annavanruiten@gmail.com"
+                  className="email-link"
+                  title="Email address"
+                >
+                  <img src={EmailLogo} />
+                </a>
+              </button>
+              <button
+                type="button"
+                className="btn btn-light button contactButton"
+              >
+                <a
+                  href="https://github.com/avr87?tab=repositories"
+                  className="github-link"
+                  title="Github-Link"
+                >
+                  <img src={GithubLogo} />
+                </a>
+              </button>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
